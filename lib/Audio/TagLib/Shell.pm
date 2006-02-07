277fbf1b3b6c214070aff753615297cda41e1b45
Dongxu Ma <dongxu.ma@gmail.com>
2006-02-07 10:36:00 +0000

    Import of DONGXU/Audio-TagLib-Shell-1.42 from CPAN.
    gitpan-cpan-distribution: Audio-TagLib-Shell
    gitpan-cpan-version:      1.42
    gitpan-cpan-path:         DONGXU/Audio-TagLib-Shell-1.42.tar.gz
    gitpan-cpan-author:       DONGXU
    gitpan-cpan-maturity:     released

--- lib/Audio/TagLib/Shell.pm
+++ lib/Audio/TagLib/Shell.pm
@@ -19,7 +19,7 @@ our @EXPORT_OK = qw(shell);
 
 our @EXPORT    = qw(shell);
 
-our $VERSION   = '1.41';
+our $VERSION   = '1.42';
 
 # support encodings
 our @ENCODING  = qw(Latin1 UTF8);
@@ -30,7 +30,7 @@ my @callback = qw(open save close title artist album comment genre
                   setGenre setYear setTrack length bitrate sampleRate
                   channels cd exit pwd ls);
 use subs map { "_".$_ } @callback;
-use subs qw(shell __complete);
+use subs qw(shell);
 
 # global default command callback map
 #{
@@ -51,215 +51,6 @@ my $ps1      = q(tag:>);
 my $encoding = undef;
 
 # Preloaded methods go here.
-
-# try to complete command line for user
-# similar to <TAB> in bash
-# no need to escape space while input
-sub __complete(@) {
-    # @_ is (current last param, entire command line, length)
-    my ($last, $cmd) = @_;
-    local *DIR;
-    if($last eq $cmd) {
-        # action only
-        # complete list is `keys %CMD' if no input
-        return sort keys %CMD if $cmd eq '';
-        # complete list grepped from `keys $CMD' 
-        return sort grep { m/^\Q$last\E/ } keys %CMD;
-    } else {
-        # action + path
-        my ($action, $path) = split / /, $cmd, 2;
-        my (@entry, @match);
-        #print "\npath = '$path'\n";
-        if($path) {
-            # return if end with a space
-            return +() if (substr($path, -1, 1) eq ' ');
-            # glob all the matched entries if possible
-            
-            if($path =~ m#^((?:/?[^/]*)*)/(.*)#o) {
-                #print "\n1 = '$1'\n2 = '$2'";
-                # $1 is basedir or null
-                return +() unless -d "$1/";
-                    
-                opendir DIR, "$1/" or do {
-                    #warn "opendir: $!";
-                    return +();
-                };
-                @entry = readdir DIR;
-                closedir DIR or warn "closedir: $!";
-                if($2) {
-                    @match = sort grep { m/^\Q$2\E/ } @entry;
-                    if(scalar(@match) == 1) {
-                        # complete will add a ' ' automatically
-                        # for a file ok
-                        # for a dir this will block the
-                        # following match
-                        # a small trick here to remove the
-                        # tail space for dir
-                        my $file = "$1/".$match[0];
-                        #print $1, "\n";
-                        
-                        my $complete;
-                        # check space in $2 and then $1
-                        # $1 will be replaced after next reg-match
-                        my $dir = $1;
-                        my $name = $2;
-                        if ($name and $name =~ m/ /o) {
-                            $complete = substr($match[0], 
-                                               rindex($name, " ")+1);
-                        } elsif ($dir and $dir =~ m/ /o) {
-                            $complete = (split / /, $dir)[-1]."/".$match[0];
-                        } else {
-                            $complete = $file;
-                        }
-                        if (-d $file) {
-                            return +($complete."/", 
-                                     $complete."/ ");
-                        } else {
-                            return $complete;
-                        }
-                    } elsif (scalar(@match) == 0) {
-                        # no match, no complete
-                        return +();
-                    } else {
-                        # grep the match list and try to get the
-                        # longest common string
-                        my ($min_match) = (sort { 
-                            length($a) <=> length($b) } @match)[0];
-                        my $min_length = length($min_match);
-                        my $common;
-                        
-                        COMMON: for (my $length =
-                                       length($2);;$length++) {
-                            
-                            if($length == $min_length) {
-                                $common = $min_match;
-                                last COMMON;
-                            }
-                            
-                            my $char = substr($match[0], $length, 1);
-                            #print "\nchar = $char\n";
-                            foreach (@match[1 .. $#match]) {
-                                if(substr($_, $length, 1) ne $char) {
-                                    $common = substr($match[0], 0,
-                                                     $length);
-                                    last COMMON;
-                                }    
-                            }
-                        }
-                        
-                        if ($2 eq $common) {
-                            # $2 is the longest common string
-                            return +(@match, undef);
-                        } else {
-                            # check space in $2 and then $1
-                            my $complete;
-                            my $dir = $1;
-                            my $name = $2;
-                            #print $name, "\n";
-                            if ($name and $name =~ m/ /o) {
-                                $complete = substr($common, 
-                                                  rindex($name, " ")+1);
-                            } elsif ($dir and $dir =~ m/ /o) {
-                                $complete = (split / /, $dir)[-1].
-                                  "/$common";
-                            } else {
-                                $complete = "$1/$common";
-                            }
-                            return +("$complete", 
-                                     "$complete ");
-                        }
-                        # NOREACH
-                    }
-                } else {
-                    return sort @entry;
-                }
-            } else {
-                # search under cwd
-                opendir DIR, "." or do {
-                    #warn "opendir: $!";
-                    return +();
-                };
-                @entry = readdir DIR;
-                closedir DIR or warn "closedir: $!";
-                @match = sort grep { m/^\Q$path\E/ } @entry;
-                if(scalar(@match) == 1) {
-                    my $file = $match[0];
-                    my $complete;
-                    # check space in $path
-                    # $1 will be replaced after next reg-match
-                    my $name = $path;
-                    if ($name and $name =~ m/ /o) {
-                        $complete = (split / /, $name)[-1]."/".$match[0];
-                    } else {
-                        $complete = $file;
-                    }
-                    if (-d $file) {
-                        return +($complete."/", 
-                                 $complete."/ ");
-                    } else {
-                        return $complete;
-                    }
-                } elsif (scalar(@match) == 0) {
-                    return +();
-                } else {
-                    # grep the match list and try to get the
-                    # longest common string
-                    my ($min_match) = (sort { 
-                        length($a) <=> length($b) } @match)[0];
-                    my $min_length = length($min_match);
-                    my $common;
-                    
-                    COMMON: for (my $length =
-                                   length($path);;$length++) {
-
-                        if($length == $min_length) {
-                            $common = $min_match;
-                            last COMMON;
-                        }
-                        
-                        my $char = substr($match[0], $length, 1);
-                        foreach (@match[1 .. $#match]) {
-                            if(substr($_, $length, 1) ne $char) {
-                                $common = substr($match[0], 0,
-                                                 $length);
-                                last COMMON;
-                            }    
-                        }
-                    }
-                    
-                    if ($path eq $common) {
-                        # $path is the longest common string
-                        return +(@match, undef);
-                    } else {
-                        # check space in $path
-                        my $complete;
-                        my $name = $path;
-                        if ($name and $name =~ m/ /o) {
-                            $complete = substr($common, 
-                                               rindex($name, " ")+1);
-                        } else {
-                            $complete = "$path/$common";
-                        }
-                        return +("$complete", 
-                                 "$complete ");
-                    }
-                    # NOREACH
-                }
-            }
-        } else {
-            # no param
-            # ls all entries under cwd
-            opendir DIR, "." or do { 
-                #warn "opendir: $!";
-                return +();
-            };
-            @entry = readdir DIR;
-            closedir DIR or warn "closedir: $!";
-            return sort grep { /^[^.]/o } @entry;
-        }
-    }
-    # NOREACH
-}
 
 # main sub exported
 # start the shell
@@ -292,17 +83,12 @@ sub shell() {
     }
     
     # open shell
-    require Term::ReadLine;
-    my $term = Term::ReadLine->new("TagLib mini shell");
-    my $attr = $term->Attribs;
-    if($term->ReadLine eq "Term::ReadLine::Gnu") {
-        $attr->{attempted_completion_function} =
-          __PACKAGE__."::__complete";
-    } else {
-        $attr->{completion_function} = __PACKAGE__. "::__complete";
-    }
+    require Term::BashTab;
+    local (*Term::BashTab::COMMAND) = \@callback;
+    
+    my $term = Term::BashTab->new("TagLib mini shell");
     my $line;
-    my $OUT = $term->OUT || \*STDOUT;
+    my $OUT  = $term->OUT || \*STDOUT;
     select $OUT;
     $| = 1;
     LOOP: while (1) {
